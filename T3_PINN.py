e2cf66f54cb72d1aa2bb8881d36c9980214273fc
h_robijns <hugojfrobijns@gmail.com>
2024-08-18 16:23:36 +0200

    polishing

--- T3_PINN.py
+++ T3_PINN.py
@@ -2,10 +2,11 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 ###################################################################################################
 # set the floating point precision
-tf.keras.backend.set_floatx('float32')
+tf.keras.backend.set_floatx('float64')
 
 ###################################################################################################
 # define a trig activitation layer - this enforces periodicity, 
@@ -33,15 +34,46 @@ class PINN:
         ])
         
 ###################################################################################################
-# define the metric, with an x-dependance
-    def g(self, x):
-        x = x[0] # reduces input from shape (1,3) to (3,) to allow it to fit into metric matrix
-        g = tf.convert_to_tensor([
-            [1.0+4*tf.sin(x[1])/2 +10*tf.sin(x[2])/4, 0.0, 0.0],
-            [0.0, 1.0+tf.sin(x[0])/2 +tf.sin(x[2])/4, 0.0],
-            [0.0, 0.0, 1.0+tf.sin(x[0])/2 +tf.sin(x[1])/2]
-        ], dtype=tf.float32)
+# define the metric, as by Kerofsky's paper, with custom R(r).
+
+    def R(self, r):
+        r = tf.convert_to_tensor(r, dtype=tf.float64)
         
+        # Kerofsky's function
+        '''
+        return tf.where(r < 1, r,  
+                tf.where(r <= 2, tf.constant(1.0, dtype=tf.float64),  
+                    1.0 + (r - 2) 
+                )
+            )
+        '''
+
+        # should learn constants for a linear function of r
+        # return r 
+
+    def g(self,x):
+        x = x[0]
+
+        r = tf.sqrt(x[0]**2 + x[1]**2 + x[2]**2)
+        theta = tf.math.atan2(r, x[2])
+        phi = tf.math.atan2(x[1], x[0])
+        Rr = self.R(r)
+        
+        # define individual matrix elements
+        g11 =  tf.square(tf.cos(phi))*tf.square(tf.sin(theta)) + tf.square((tf.sin(theta)*tf.sin(phi)) * Rr) + tf.square((tf.cos(theta)*tf.cos(phi)) * Rr)
+        print(g11)
+        g12 = tf.square(tf.sin(theta))*tf.sin(phi)*tf.cos(phi) + tf.square(tf.cos(theta) * Rr)*tf.sin(phi)*tf.cos(phi) - tf.square(tf.sin(theta) * Rr)*tf.sin(phi)*tf.cos(phi)
+        g13 = tf.cos(theta)*tf.sin(theta)*tf.cos(phi) - tf.square(Rr)*tf.sin(theta)*tf.cos(theta)*tf.cos(phi)
+        g22 = tf.square(tf.sin(phi))*tf.square(tf.sin(theta)) + tf.square((tf.cos(theta)*tf.sin(phi)) * Rr) + tf.square((tf.sin(theta)*tf.cos(phi)) * Rr)
+        g23 = tf.cos(theta)*tf.sin(theta)*tf.sin(phi) - tf.square(Rr)*tf.sin(theta)*tf.cos(theta)*tf.sin(phi)
+        g33 = tf.square(tf.cos(theta))+ tf.square(Rr*tf.sin(theta))
+
+        # return the matrix
+        g = tf.convert_to_tensor([
+            [g11,g12,g13],
+            [g12,g22,g23],
+            [g13,g23,g33]
+        ], dtype=tf.float64)
         return g
 
 ###################################################################################################
@@ -49,7 +81,7 @@ class PINN:
 
     def evaluate(self, inputs): 
     # function which passes an input through the model
-        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
+        inputs = tf.convert_to_tensor(inputs, dtype=tf.float64)
         outputs = self.model(inputs)
         return outputs
     
@@ -57,18 +89,22 @@ class PINN:
     # define the Hodge operator, defined differently depending on which rank form it is acting on 
   
         if rank_acting_on == 1: # Hodge star acting on a 1-form: output is a 2-form
+            u1, u2, u3 = inputs[:, 0:1], inputs[:, 1:2], inputs[:, 2:3]
+            u = tf.concat([u1, -u2, u3], axis=1)
             sqrt_det_metric = tf.sqrt(tf.abs(tf.linalg.det(g)))
             inv_metric = tf.linalg.inv(g)
-            product = sqrt_det_metric * tf.matmul(inputs, inv_metric)
+            product = sqrt_det_metric * tf.matmul(u, inv_metric)
             return tf.concat([product[:, 0:1], # coefficient of dx2 ^ dx3
                           product[:, 1:2], # coefficient of dx3 ^ dx1
                           product[:, 2:3]], # coefficient of dx1 ^ dx2
                           axis=1)
         
         elif rank_acting_on == 2: # Hodge star acting on a 2-form: output is a 1-form
+            u1, u2, u3 = inputs[:, 0:1], inputs[:, 1:2], inputs[:, 2:3]
+            u = tf.concat([u1, -u2, u3], axis=1)
             sqrt_det_metric = tf.sqrt(tf.abs(tf.linalg.det(g)))
             inv_metric = tf.linalg.inv(g)
-            product = sqrt_det_metric * tf.matmul(inputs, inv_metric)
+            product = sqrt_det_metric * tf.matmul(u, inv_metric)
             return tf.concat([product[:, 0:1], # coefficient of dx1
                           product[:, 1:2], # coefficient of dx2
                           product[:, 2:3]], # coefficient of dx3
@@ -76,13 +112,13 @@ class PINN:
             
         elif rank_acting_on == 3: # Hodge star acting on a 3-form: output is a 0-form
             sqrt_det_metric = tf.sqrt(tf.abs(tf.linalg.det(g)))
-            return (1/sqrt_det_metric) * inputs 
+            return sqrt_det_metric * inputs 
 
     def exterior_derivative(self, inputs, x, tape, rank_acting_on):
     # define the exterior derivative, defined differently depending on which rank form it is acting on
 
-        if rank_acting_on == 0: # exterior derivative of a 0-form: output is a 3-form
-            return tape.gradient(inputs, x)
+        if rank_acting_on == 0: # exterior derivative of a 0-form: output is a 1-form
+            return tape.gradient(inputs, x) # coefficients of dx1, dx2 and dx3
 
         elif rank_acting_on == 1: # exterior derivative of a 1-form: output is a 2-form
             derivatives = tf.concat([
@@ -99,12 +135,12 @@ class PINN:
         
         elif rank_acting_on == 2: # exterior derivative of a 2-form: output is a 3-form
             derivatives = tf.concat([
-                tape.gradient(inputs[:, 0], x),
+                tape.gradient(inputs[:, 0], x), 
                 tape.gradient(inputs[:, 1], x),
                 tape.gradient(inputs[:, 2], x),
             ], axis=1) 
 
-            return tf.expand_dims(derivatives[:, 0] - derivatives[:, 4] + derivatives[:, 8], axis=1)
+            return tf.expand_dims(derivatives[:, 0] - derivatives[:, 4] + derivatives[:, 8], axis=1) 
 
     def calculate_PDE_error(self, x, tape):
     # function which calculates overall laplacian of the 1-form, as the sum of two terms
@@ -131,7 +167,7 @@ class PINN:
     def loss(self, x_collocation, tape):
         PDE_errors = tf.vectorized_map(lambda x: self.calculate_PDE_error(x, tape), x_collocation)
         norm_factor = tf.reduce_sum(tf.abs(self.model(x_collocation)))
-        loss = tf.reduce_mean(tf.square(PDE_errors)) / norm_factor
+        loss = tf.reduce_mean(PDE_errors) / norm_factor
         return loss
     
 ###################################################################################################
@@ -159,59 +195,73 @@ class PINN:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
+        # Make the grid
         x, y, z = np.meshgrid(np.arange(0, 1.1, 0.2),
                               np.arange(0, 1.1, 0.2),
                               np.arange(0, 1.1, 0.2))
         grid_points = np.stack([x.ravel(), y.ravel(), z.ravel()], axis=-1)
-        grid_tensor = tf.convert_to_tensor(grid_points, dtype=tf.float32)
+        grid_tensor = tf.convert_to_tensor(grid_points, dtype=tf.float64)
 
+        # Evaluate the model
         u = self.evaluate(grid_tensor).numpy()
 
-        u1 = u[:, 0].reshape(x.shape)
-        u2 = u[:, 1].reshape(x.shape)
-        u3 = u[:, 2].reshape(x.shape)
-
-        ax.quiver(x, y, z, u1, u2, u3, length=0.1, normalize=True)
+        # Reshape to match grid
+        u1 = u[:, 0].reshape(x.shape)*0.2
+        u2 = u[:, 1].reshape(x.shape)*0.2
+        u3 = u[:, 2].reshape(x.shape)*0.2
+        ax.quiver(x, y, z, u1, u2, u3, normalize=False)
 
         plt.show()
 
 ###################################################################################################
-#  simple check for zeroes
+# simple check for zeroes: first a random check, then minimise from there.
 
     def find_zero_vector(self, num_points=1e6):
         # generate random points within the unit cube
         random_points = np.random.uniform(low=0, high=1, size=(int(num_points), 3))
-        random_points_tensor = tf.convert_to_tensor(random_points, dtype=tf.float32)
+        random_points_tensor = tf.convert_to_tensor(random_points, dtype=tf.float64)
         
+        # function to calculate the metric norm
         def calculate_metric_norm(x):
             x = tf.expand_dims(x, axis=0)  # adjust shape to match the metric function input
             g = self.g(x)  # get the metric tensor for the single point
-            
-            # evaluate the model on these points
             u = self.evaluate(x)[0]  # get the output vector (1-form)
-            
-            # reshape u to be a column vector
-            u = tf.reshape(u, (3, 1))
+            u = tf.reshape(u, (3, 1)) # reshape
             
             # calculate the metric norm using g_ij u^i u^j
             metric_norm_squared = tf.matmul(tf.matmul(tf.transpose(u), g), u)
             metric_norm = tf.sqrt(metric_norm_squared)
-            
-            # return a scalar
-            return tf.squeeze(metric_norm)
-
-        # calculate norms using vectorized_map for speed
+            return tf.squeeze(metric_norm) # return a scalar
+        
         norms = tf.vectorized_map(calculate_metric_norm, random_points_tensor).numpy()
 
-        # check if any norm is close to zero
-        zero_vector_indices = np.where(norms < 1e-3)[0]
-        
-        if len(zero_vector_indices) > 0:
-            print("Found zero vectors! Yipeee")
+        # find the index of the minimum norm
+        min_index = np.argmin(norms)
+        min_norm = norms[min_index]
+        min_point = random_points[min_index]
+
+        # print the result of the random search
+        if min_norm < 1e-3:
+            print(f"Found a point with small norm at {min_point} with norm {min_norm} during random search.")
         else:
-            print("No zero vectors found. :( Boohoo")
-        
-        print("Smallest norm: " + str(np.min(norms)))
+            print("No point with sufficiently small norm found during random search. Smallest norm found during random search: " + str(min_norm))
+    
+        # define the objective function for minimization
+        def objective_function(x):
+            x_tensor = tf.convert_to_tensor(np.expand_dims(x, axis=0), dtype=tf.float64)
+            g = self.g(x_tensor)
+            u = self.evaluate(x_tensor)[0]
+            u = tf.reshape(u, (3, 1))
+            metric_norm_squared = tf.matmul(tf.matmul(tf.transpose(u), g), u)
+            metric_norm = tf.sqrt(metric_norm_squared)
+            return tf.squeeze(metric_norm).numpy()
+
+        # perform the minimization starting from the best point found
+        result = minimize(objective_function, min_point, method='Nelder-Mead')
+
+        # get the point with the smallest norm found
+        min_norm_final = result.fun
+        print("Smallest norm found after minimization: " + str(min_norm_final))
 
 ###################################################################################################
 #  running the program
@@ -221,20 +271,20 @@ if __name__ == '__main__':
     # generate collocation points
     num_samples = 1000
     x_collocation = np.random.uniform(low=0, high=1, size=(num_samples, 3))
-    x_collocation = tf.convert_to_tensor(x_collocation, dtype=tf.float32)
+    x_collocation = tf.convert_to_tensor(x_collocation, dtype=tf.float64)
 
     # initialise and train network
     pinn = PINN()
-    pinn.train(x_collocation, epochs=1000, learning_rate=0.001)
+    pinn.train(x_collocation, epochs=500, learning_rate=0.001)
     
     # check for periodicity (should always be true)
-    inputs = np.array([[1, 1, 1], [2, 2, 2]], dtype=np.float32)
+    inputs = np.array([[1, 1, 1], [2, 2, 2]], dtype=np.float64)
     outputs = pinn.evaluate(inputs)
     print(f"Outputs for [1, 1, 1] and [2, 2, 2]:\n{outputs.numpy()} (these should be exactly the same if the NN has learnt a periodic solution)")
 
     # check for constant output (should be true for the identity metric)
     random_inputs = np.random.uniform(low=0, high=1, size=(5, 3))
-    random_inputs_tensor = tf.convert_to_tensor(random_inputs, dtype=tf.float32)
+    random_inputs_tensor = tf.convert_to_tensor(random_inputs, dtype=tf.float64)
     random_outputs = pinn.evaluate(random_inputs_tensor).numpy()
     print("Random inputs:")
     print(random_inputs)
